3b518a160ba6267601726c29f619669b05bd2ca7
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-08-01 09:34:17 +0300

    Post Data from server

--- src/router.js
+++ src/router.js
@@ -5,8 +5,10 @@ const router = (req, res) => {
 
   if (endpoint === '/') {
     logic.serveHome(res, '/index.html');
-  } else if (['/favicon.ico', '/css/style.css', '/js/dom.js', '/js/request.js'].includes(endpoint)) {
+  } else if (['/favicon.ico', '/css/style.css', '/js/dom.js', '/js/request.js', '/js/fetch.js'].includes(endpoint)) {
     logic.serveHome(res, endpoint);
+  } else if (endpoint === '/weather'){
+    logic.serveAPI(req,res);
   } else {
     logic.pageNotFound(res);
   }
